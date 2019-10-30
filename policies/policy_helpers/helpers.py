063a95640580a140b81175f959d35e623d034fa6
gwbcho <gregwbcho@gmail.com>
2019-10-30 18:12:58 -0400

    GAC-005 Corrected several issues in the original Policy class implementation.

--- policies/policy_helpers/helpers.py
+++ policies/policy_helpers/helpers.py
@@ -36,16 +36,22 @@ def hard_update(target, source):
 def normalize(x, stats):
     """
     Note: Not sure if this is correct
+
+    Args:
+        x (tf.Variable): tensorflow variable to normalize
+        stats (utils.RunningMeanStd): Information regarding the mean and variance of data
+
+    Returns:
+        normalized value of x
     """
     if stats is None:
         return x
     mean, variance = tf.moments(stats, axes=1)
-    return (x - tf.Variable(mean)) / tf.Variable(tf.sqrt(variance))
+    return (x - tf.Variable(stats.mean)) / tf.Variable(tf.sqrt(stats.var))
 
 
 def denormalize(x, stats):
     if stats is None:
         return x
-    mean, variance = tf.moments(stats, axes=1)
-    sigma = tf.sqrt(variance)
-    return x * sigma + mean
+    sigma = tf.sqrt(stats.var)
+    return x * sigma + stats.mean
