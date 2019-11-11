2326c318d8b495615cad52a6b5e0410617542ceb
gwbcho <gregwbcho@gmail.com>
2019-11-11 13:56:36 -0500

    GAC-000 Removed print.

--- tests/unit/policy/networks.py
+++ tests/unit/policy/networks.py
@@ -85,7 +85,6 @@ class TestGacNetworks(unittest.TestCase):
             )
         )
         action = actor(state, taus, prev_action)
-        print(action)
         self.assertEqual(action.shape[0], batch_size_1)
         self.assertEqual(action.shape[1], action_dim)
 
