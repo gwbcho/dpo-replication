0c74b044798eeab2e96688d3714073add437fc44
LiuYangMage <liu_yang@brown.edu>
2019-11-15 00:23:17 -0500

    Don't care about average reward.

--- main.py
+++ main.py
@@ -107,27 +107,29 @@ def main():
     """
     average_rewards = 0
     count = 0
+    episode_count = 0
     for t in range(args.T):
         """
         Get an action from neural network and run it in the environment
         """
-        if t%10 == 0:
-            print('t =', t)
+        # if t%10 == 0:
+        #     print('t =', t)
         action = gac.get_action(tf.convert_to_tensor([state]))
         # remove the batch_size dimension if batch_size == 1
         action = tf.squeeze(action, [0]).numpy()
         next_state, reward, is_terminal, _ = env.step(action)
         # env.render()
         gac.store_transitions(state, action, reward, next_state, is_terminal)
-        average_rewards = average_rewards + ((reward - average_rewards)/(count + 1))
-        count += 1
+        # average_rewards = average_rewards + ((reward - average_rewards)/(count + 1))
+        # count += 1
         # print('average_rewards:', average_rewards)
 
         # check if game is terminated to decide how to update state, episode_steps, episode_rewards
         if is_terminal:
             state = env.reset()
+            episode_count += 1
             results_dict['train_rewards'].append((t, episode_rewards))
-            print('time: {} train eposide reward: {}'.format(t, episode_rewards))
+            print('training episode: {}, total interactions: {}, reward: {}'.format(episode_count, t, episode_rewards))
             episode_steps = 0
             episode_rewards = 0
         else:
