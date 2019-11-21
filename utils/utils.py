a07bbcc79b6b86c56e258cc47d2743b17e80d04f
gwbcho <gregwbcho@gmail.com>
2019-11-20 22:39:54 -0500

    GAC-000 Saved model.

--- utils/utils.py
+++ utils/utils.py
@@ -8,10 +8,10 @@ def save_model(actor, basedir=None):
     if not os.path.exists('models/'):
         os.makedirs('models/')
 
-    actor_path = "{}/ddpg_actor".format(basedir)
+    actor_path = "{}/actor".format(basedir)
 
     # print('Saving models to {} {}'.format(actor_path, adversary_path))
-    tf.save_model.save(actor, actor_path)
+    tf.saved_model.save(actor, actor_path)
 
 
 def load_model(basedir=None):
