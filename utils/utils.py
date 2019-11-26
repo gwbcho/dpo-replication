GAC-000 Final fixes to get normalization to work

utils/utils.py
@@ -40,13 +40,13 @@ def update_mean_var_count_from_moments(mean, var, count, batch_mean, batch_var, 
 class RunningMeanStd(object):
     # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
     def __init__(self, epsilon=1e-4, shape=()):
-        self.mean = np.zeros(shape, 'float64')
-        self.var = np.ones(shape, 'float64')
+        self.mean = tf.zeros(shape, 'float32')
+        self.var = tf.ones(shape, 'float32')
         self.count = epsilon
 
     def update(self, x):
-        batch_mean = np.mean(x, axis=0)
-        batch_var = np.var(x, axis=0)
+        batch_mean = tf.dtypes.cast(tf.math.reduce_mean(x, axis=0), tf.dtypes.float32)
+        batch_var = tf.dtypes.cast(tf.math.square(tf.math.reduce_std(x, axis=0)), tf.dtypes.float32)
         batch_count = x.shape[0]
         self.update_from_moments(batch_mean, batch_var, batch_count)
 